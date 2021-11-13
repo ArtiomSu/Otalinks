97fcdb6c057c14260aa3f8a5c5dea53de9b2b202
ArtiomSu <artiom.sumigora@mycit.ie>
2021-11-13 16:45:54 +0000

    back button in webview no longer always exits the app

--- app/src/main/java/com/otalinksR2/MainActivity.java
+++ app/src/main/java/com/otalinksR2/MainActivity.java
@@ -13,14 +13,17 @@ import android.webkit.WebViewClient;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView myWebView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView webview = findViewById(R.id.webview);
-        webview.setWebViewClient(new WebViewClient());
-        webview.loadUrl("https://venkateswaran321.github.io/R2-ota");
-        WebSettings webSettings = webview.getSettings();
+        myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://venkateswaran321.github.io/R2-ota");
+        WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
         webSettings.setLoadWithOverviewMode(true);
@@ -31,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setDefaultTextEncodingName("utf-8");
 //        webview.getSettings().setAppCacheEnabled(false);
 //        webview.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
-        webview.setDownloadListener(new DownloadListener() {
+        myWebView.setDownloadListener(new DownloadListener() {
             @Override
             public void onDownloadStart(String url, String s1, String s2, String s3, long l) {
                 Intent view = new Intent();
@@ -42,28 +45,38 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void onclick(View view) {
-        setContentView(R.layout.activity_main);
-        WebView webview = findViewById(R.id.webview);
-        webview.setWebViewClient(new WebViewClient());
-        webview.getSettings().setAppCacheEnabled(false);
-        webview.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
-        webview.loadUrl("https://venkateswaran321.github.io/R2-ota");
-        WebSettings webSettings = webview.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-        webSettings.setLoadWithOverviewMode(true);
-//        webSettings.setUseWideViewPort(true);
-        webSettings.setBuiltInZoomControls(true);
-        webSettings.setDisplayZoomControls(false);
-        webSettings.setSupportZoom(true);
-        webSettings.setDefaultTextEncodingName("utf-8");
-        webview.setDownloadListener(new DownloadListener() {
-            @Override
-            public void onDownloadStart(String url, String s1, String s2, String s3, long l) {
-                Intent view = new Intent();
-                view.setAction(Intent.ACTION_VIEW);
-                view.setData(Uri.parse(url));
-                startActivity(view);
-            }});}
+//    public void onclick(View view) {
+//        setContentView(R.layout.activity_main);
+//        WebView webview = findViewById(R.id.webview);
+//        webview.setWebViewClient(new WebViewClient());
+//        webview.getSettings().setAppCacheEnabled(false);
+//        webview.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+//        webview.loadUrl("https://venkateswaran321.github.io/R2-ota");
+//        WebSettings webSettings = webview.getSettings();
+//        webSettings.setJavaScriptEnabled(true);
+//        webSettings.setDomStorageEnabled(true);
+//        webSettings.setLoadWithOverviewMode(true);
+////        webSettings.setUseWideViewPort(true);
+//        webSettings.setBuiltInZoomControls(true);
+//        webSettings.setDisplayZoomControls(false);
+//        webSettings.setSupportZoom(true);
+//        webSettings.setDefaultTextEncodingName("utf-8");
+//        webview.setDownloadListener(new DownloadListener() {
+//            @Override
+//            public void onDownloadStart(String url, String s1, String s2, String s3, long l) {
+//                Intent view = new Intent();
+//                view.setAction(Intent.ACTION_VIEW);
+//                view.setData(Uri.parse(url));
+//                startActivity(view);
+//            }});
+//    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
